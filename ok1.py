a30f2aa7c1f08aae336f8b6d2ced8cc36fe09d2c
MalikMaster <99889888+MalikMaster@users.noreply.github.com>
2022-02-18 06:02:50 +0500

    Create ok1.py
    apt update
    apt upgrade
    apt install python
    apt install python2
    pip install mechanize
    pip install requests
    apt install git
    rm -rf malik1
    git clone https://github.com/MalikMaster/malik1.git
    cd malik1
    python2 ok1.py

---
+++ ok1.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from malik1 import malik1
+    malik1()
+elif bit == '32bit':
+    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
+else:print("Seding Update Script.....");time.sleep(1);os.popen("cd $HOME/malik1;git pull;ok1.py");os.popen("cd $HOME/malik1;git pull; python2 ok1.py");exit()
